2e80585103365b5ad8b71d09ede6472a400e0774
Somya <somya0610@gmail.com>
2021-03-23 14:50:02 -0700

    Added 'Apply' button for movie filters on home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -19,6 +19,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemText from '@material-ui/core/ListItemText';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 
 const styles = theme => ({
@@ -164,6 +165,9 @@ class Home extends Component {
                                         type="date"
                                         defaultValue=""
                                         InputLabelProps={{ shrink: true }} />
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <Button variant="contained" color="primary">APPLY</Button>
                                 </FormControl>
                             </CardContent>
                         </Card>
